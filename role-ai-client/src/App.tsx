9befe2b969f93accebe0d284ca656e5e83d5bbe8
Nika Sakandelidze <nikasakana@gmail.com>
2023-09-12 17:24:25 +0400

    Minor update

--- role-ai-client/src/App.tsx
+++ role-ai-client/src/App.tsx
@@ -1,27 +1,16 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { RouteData, routes } from "./routes";
 
 const App = () => {
-    return (
-        <div
-            className="App"
-            style={{
-                height: "100vh",
-                width: "100%",
-            }}
-        >
-            <Routes>
-                {routes.map((route: RouteData) => (
-                    <Route
-                        key={route.id}
-                        path={route.path}
-                        element={route.element}
-                    />
-                ))}
-            </Routes>
-        </div>
-    );
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <Routes>
+        {routes.map((route: RouteData) => (
+          <Route key={route.id} path={route.path} element={<route.element />} />
+        ))}
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
