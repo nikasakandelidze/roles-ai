6ae3bee24c224b283978aaef5e2a5e7146b7a396
Nika Sakandelidze <nikasakana@gmail.com>
2023-09-27 17:50:56 +0400

    Added database and api separate k8s deployments

--- role-ai-api/src/health-check/health-check.controller.ts
+++ role-ai-api/src/health-check/health-check.controller.ts
@@ -1,9 +1,16 @@
 import { Controller, Get } from "@nestjs/common";
 
+const id = Math.random();
+
 @Controller()
 export class HealthcheckController {
   @Get("/api/health-check")
   healthCheck() {
     return { ok: true };
+  }
+
+  @Get("/api/check")
+  check() {
+    return { result: `This is server with id: ${id}` };
   }
 }
